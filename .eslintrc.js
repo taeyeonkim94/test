82de932ed9aa2010f6a9357e5da87896a751461c
TaeyeonKim <gggm0117@naver.com>
2024-12-27 10:11:13 +0900

    eslint, prettier 세팅 완료

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  parser: '@typescript-eslint/parser', // TypeScript 파서 사용
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'airbnb-base', // Airbnb 기본 스타일 사용
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    // 필요에 따라 규칙을 오버라이드 할 수 있습니다.
+    'no-console': 'off', // 콘솔 사용을 허용 (예시로 설정)
+    '@typescript-eslint/no-unused-vars': ['error'], // 사용되지 않는 변수는 에러
+    'import/no-unresolved': 'off', // import 관련 에러를 끔 (예: 별칭 경로 사용시)
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }], // devDependencies 허용
+    'class-methods-use-this': 'off',
+    'no-useless-constructor': 'off', // 생성자에 대한 불필요한 경고 비활성화
+    'no-empty-function': 'off'
+  }
+};
